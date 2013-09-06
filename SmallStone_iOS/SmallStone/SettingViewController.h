2fc272c2cbd97c45567c178ea17e356a2221d4b8
ZhengQingpeng <andy5920@gmail.com>
2013-09-06 19:41:13 +0800

    增加设置入口

---
+++ SmallStone_iOS/SmallStone/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  SmallStone
+//
+//  Created by Andy on 13-9-6.
+//  Copyright (c) 2013年 tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
