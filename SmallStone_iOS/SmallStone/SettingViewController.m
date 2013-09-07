edd3ad431dab27fed783f9030269af01737eb3ef
ZhengQingpeng <andy5920@gmail.com>
2013-09-07 18:23:27 +0800

    bak

--- SmallStone_iOS/SmallStone/SettingViewController.m
+++ SmallStone_iOS/SmallStone/SettingViewController.m
@@ -12,6 +12,8 @@
 
 @end
 
+#define saveNicknameURL @"http://180.153.0.208/index.php?o=save&id=:uuid&name=:nickname&token=google"
+
 @implementation SettingViewController
 
 @synthesize nickname;
@@ -51,6 +53,26 @@
 	[defaults setObject:nickname.text forKey:@"nickname"];
 	[defaults synchronize];
 	//NSLog(@"%@", [defaults objectForKey:@"serviceIp"]);
+	//请求后台创建该用户名
+	//NSString *appUrl = [[NSString alloc] initWithFormat:@"%@", saveNicknameURL];
+	NSMutableString *appUrl = [[NSMutableString alloc] initWithString:saveNicknameURL];
+	[appUrl appendString:@"&id="];
+	//[appUrl appendString:@""]
+	NSLog(@"%@", appUrl);
+	
+}
+
+-(void)sendCreateData
+{
+	
+    /*
+    NSString *appUrl = [[NSString alloc] initWithFormat:@"%@", saveNicknameURL];
+    NSURL *url = [NSURL URLWithString:appUrl];
+    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:30];
+    NSURLConnection *urlConnection = [NSURLConnection connectionWithRequest:urlRequest delegate:self];
+    [urlConnection start];
+    [self showNetworkActivityIndicator];
+	*/
 }
 
 -(void) textFieldDoneEditing:(id)sender
