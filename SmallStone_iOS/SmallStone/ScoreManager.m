d3a9aa064d63841a208dcae3e7a9ca8ffd7c24d6
jaminzzhang <jaminzzhang@gmail.com>
2013-09-08 13:44:15 +0800

    上报时，level需从1开始。

--- SmallStone_iOS/SmallStone/ScoreManager.m
+++ SmallStone_iOS/SmallStone/ScoreManager.m
@@ -114,7 +114,7 @@
     const char * cUserName = [[userName stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding] UTF8String];
     NSString * encodeDeviceId = [[CommonUtility getDeviceId] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
     NSString *appUrl = [NSString stringWithFormat:@"http://180.153.0.208/index.php?o=save"
-                        @"&id=%@&name=%s&score=%d&level=%d", encodeDeviceId, cUserName, self.totalScore, self.topLevel];
+                        @"&id=%@&name=%s&score=%d&level=%d", encodeDeviceId, cUserName, self.totalScore, self.topLevel + 1];
     NSURL *url = [NSURL URLWithString:appUrl];
 	NSLog(@"%@", url);
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringCacheData timeoutInterval:10];
